3c57d1c1a60fe4b3e953bec6ae243aff821c54bc
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2023-11-27 12:55:56 -0300

    more configs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './contexts/Theme/index.tsx'
+import { myTheme } from './theme/index.ts'
+import ToDoProvider from './contexts/ToDo/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={myTheme}>
+      <ToDoProvider>
+        <App />
+      </ToDoProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
